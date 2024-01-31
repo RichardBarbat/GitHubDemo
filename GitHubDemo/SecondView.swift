de373f242f7279e20bc20c088b02d8017c116a06
Richard Barbat <info@richard-barbat.de>
2024-01-31 14:19:46 +0100

    Added SecondView.

---
+++ GitHubDemo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Student on 31.01.24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
