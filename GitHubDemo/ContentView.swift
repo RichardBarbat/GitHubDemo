6a778e80955470e341214f2dc6e4b7cf3bc1fc05
Richard Barbat <info@richard-barbat.de>
2024-01-31 14:02:12 +0100

    Changes on ContentView.

--- GitHubDemo/ContentView.swift
+++ GitHubDemo/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    //MARK: - Body
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is a Test!")
         }
         .padding()
     }
